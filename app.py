d4b4e5bea2916792a55c20784ade53964bd4bdaf
udgeeth521 <54429185+udgeeth521@users.noreply.github.com>
2026-04-09 13:00:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return '<h1>DevOps Pipeline Success!</h1><p>The Python App is running on the Docker Node.</p>'
-
-if __name__ == "__main__":
-    # It MUST be 0.0.0.0 to be accessible outside the container
-    app.run(host='0.0.0.0', port=8080)
+FROM python:3.9-slim
+WORKDIR /app
+COPY . /app
+RUN pip install flask
+EXPOSE 8080
+CMD ["python", "app.py"]
