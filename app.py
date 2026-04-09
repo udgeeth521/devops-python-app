8c74093963fab216e0fb66ca7847ba3d68d452f7
udgeeth521 <54429185+udgeeth521@users.noreply.github.com>
2026-04-09 12:55:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-print("DevOps Pipeline Success!")
-print("The Python App is running on the Docker Node.")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1>DevOps Pipeline Success!</h1><p>The Python App is running on the Docker Node.</p>'
+
+if __name__ == "__main__":
+    # It MUST be 0.0.0.0 to be accessible outside the container
+    app.run(host='0.0.0.0', port=8080)
